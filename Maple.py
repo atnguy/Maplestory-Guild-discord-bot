c19275cc77e21d26a79d3b3d8f5c881f6d28b15b
atnguy <diryathing@gmail.com>
2021-09-10 19:43:08 -0400

    Corrected intervals values

--- Maple.py
+++ Maple.py
@@ -11,7 +11,7 @@ bot = commands.Bot(command_prefix='~', case_insensitive = True)
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 help_description = "Sends you pings reminding you to pop your legion wealth or when your totem is about to expire. \nEnter a number 1-5 in [Presets] to use a certain preset as described below. (If none is entered, preset 1 is used by default) \n" + \
-"1: 4 30 minute legion wealth coupons\n 2: 2 30 minute and 3 20 minute legion wealth coupons \n 3: 6 20 minute legion wealth coupons \n 4: 1 20 minute legion wealth coupon and 10 10 minute legion wealth coupons"
+"1: 4 30 minute legion wealth coupons\n2: 2 30 minute and 3 20 minute legion wealth coupons \n3: 6 20 minute legion wealth coupons \n4: 1 20 minute legion wealth coupon and 10 10 minute legion wealth coupons"
 @bot.command(name='totem',help =help_description)
 async def totem(ctx,preset: int=None):
     num = 4
@@ -24,17 +24,22 @@ async def totem(ctx,preset: int=None):
         if(preset == 3):
             num = 6
             loop = 6
-            intervals = 900
+            intervals = 1200
         if(preset == 4):
             num = 11
             loop = 11
-            intervals = 900
+            intervals = 1200
     await ctx.send(f"{ctx.author.mention}, your totem session has started. I will ping you within the next 2 hours for reminders.")
     while(loop > 0):
         await asyncio.sleep(intervals)
         if(num > 0):
             await ctx.send(f"{ctx.author.mention} your legion wealth has expired")
             num -= 1
+        if(preset != None):
+            if(preset == 2 and num == 3):
+                intervals = 1200
+            if(preset == 4 and num == 10):
+                intervals = 600
         loop -= 1
 
     await ctx.send(f"{ctx.author.mention} your totem session has expired")
